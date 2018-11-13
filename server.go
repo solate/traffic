d9697b3f3803db35edffd535e2c86484d39d20b1
solate <349370626@qq.com>
2018-11-13 14:02:14 +0800

    搭建一个web server

---
+++ server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	r := gin.Default()
+
+
+	//添加静态文件
+	r.Use(static.Serve("/", static.LocalFile("./views", true)))
+
+
+	//设置首页
+	r.GET("/", func(c *gin.Context) {
+		c.Redirect(http.StatusFound, "/index.html")
+	})
+
+	//其他页
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080
+}
